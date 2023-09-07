6be258b3e6a47960f5a577ee4f175d519dd36f79
Uriel Vargas <urielvc22@gmail.com>
2023-09-07 10:12:59 -0400

    cambios

--- app.js
+++ app.js
@@ -42,4 +42,6 @@ app.get('/generic', function (req, res) {
     res.sendFile( __dirname + '/public/404.html');
   })
 
-app.listen(port)
+app.listen(port, () =>{
+  console.log(`Example app listening at http://localhost:${port}`)
+})
